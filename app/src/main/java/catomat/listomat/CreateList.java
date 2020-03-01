aea5ebbf2121355534f2d43f2300fb9b932caa69
cat-o-mat <bstefanowa@gmail.com>
2020-03-01 12:06:28 +0100

    Input fields have hints

--- app/src/main/java/catomat/listomat/CreateList.java
+++ app/src/main/java/catomat/listomat/CreateList.java
@@ -8,21 +8,11 @@ import android.widget.Button;
 
 public class CreateList extends AppCompatActivity {
 
-    Button testBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_list);
-
-        testBtn = findViewById(R.id.testBtnCreateList);
-
-        testBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                System.out.println("Button in Create List is working.");
-            }
-        });
 
     }
 }
